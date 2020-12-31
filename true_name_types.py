5f9386e1e30b876dbec7b63f003918f527d16995
EdwardElensky <elensky_ed516@mail.ru>
2021-01-01 00:22:40 +0300

    add names and types

---
+++ true_name_types.py
@@ -0,0 +1,27 @@
+# переменные должны бить понятны
+# пишутся строчными буквами, слова в разделяются _
+person_name = 'Max'
+
+# в этом языке динамическая типизация в отличии от свифт и жс!
+# тип назначается при создании переменной автоматически
+
+# определить тип переменной можно так:
+print(type(person_name))
+# это <class 'str'>
+
+age = 30
+print(type(age))
+# <class 'int'>
+
+period = 3.2
+print(type(period))
+# <class 'float'>
+
+is_good = True
+print(type(is_good))
+# <class 'bool'>
+
+person = None
+print(type(person))
+# <class 'NoneType'>
+
