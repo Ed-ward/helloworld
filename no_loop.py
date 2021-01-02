3e48d26cfb43e16cf1f39d15010b3555f0ac314b
EdwardElensky <elensky_ed516@mail.ru>
2021-01-03 00:50:55 +0300

    add answering without loop

---
+++ no_loop.py
@@ -0,0 +1,103 @@
+# задаем вопросы без цикла
+# пока не получим правильный ответ:
+# (так себе затея, делать бесконечно не получится)
+name = input('Кто создатель python? ')
+if name == 'Гвидо':
+    print('Правильно!')
+else:
+    print('Неверно')
+    name = input('Кто создатель python? ')
+    if name == 'Гвидо':
+        print('Правильно!')
+    else:
+        print('Неверно')
+        name = input('Кто создатель python? ')
+        if name == 'Гвидо':
+            print('Правильно!')
+        else:
+            print('Неверно')
+            name = input('Кто создатель python? ')
+            if name == 'Гвидо':
+                print('Правильно!')
+            else:
+                print('Неверно')
+                name = input('Кто создатель python? ')
+                if name == 'Гвидо':
+                    print('Правильно!')
+                else:
+                    print('Неверно')
+                    name = input('Кто создатель python? ')
+                    if name == 'Гвидо':
+                        print('Правильно!')
+                    else:
+                        print('Неверно')
+                        name = input('Кто создатель python? ')
+                        if name == 'Гвидо':
+                            print('Правильно!')
+                        else:
+                            print('Неверно')
+                            name = input('Кто создатель python? ')
+                            if name == 'Гвидо':
+                                print('Правильно!')
+                            else:
+                                print('Неверно')
+                                name = input('Кто создатель python? ')
+                                if name == 'Гвидо':
+                                    print('Правильно!')
+                                else:
+                                    print('Неверно')
+                                    name = input('Кто создатель python? ')
+                                    if name == 'Гвидо':
+                                        print('Правильно!')
+                                    else:
+                                        print('Неверно')
+                                        name = input('Кто создатель python? ')
+                                        if name == 'Гвидо':
+                                            print('Правильно!')
+                                        else:
+                                            print('Неверно')
+                                            name = input('Кто создатель python? ')
+                                            if name == 'Гвидо':
+                                                print('Правильно!')
+                                            else:
+                                                print('Неверно')
+                                                name = input('Кто создатель python? ')
+                                                if name == 'Гвидо':
+                                                    print('Правильно!')
+                                                else:
+                                                    print('Неверно')
+                                                    name = input('Кто создатель python? ')
+                                                    if name == 'Гвидо':
+                                                        print('Правильно!')
+                                                    else:
+                                                        print('Неверно')
+                                                        name = input('Кто создатель python? ')
+                                                        if name == 'Гвидо':
+                                                            print('Правильно!')
+                                                        else:
+                                                            print('Неверно')
+                                                            name = input('Кто создатель python? ')
+                                                            if name == 'Гвидо':
+                                                                print('Правильно!')
+                                                            else:
+                                                                print('Неверно')
+                                                                name = input('Кто создатель python? ')
+                                                                if name == 'Гвидо':
+                                                                    print('Правильно!')
+                                                                else:
+                                                                    print('Неверно')
+                                                                    name = input('Кто создатель python? ')
+                                                                    if name == 'Гвидо':
+                                                                        print('Правильно!')
+                                                                    else:
+                                                                        print('Неверно')
+                                                                        name = input('Кто создатель python? ')
+                                                                        if name == 'Гвидо':
+                                                                            print('Правильно!')
+                                                                        else:
+                                                                            print('Неверно')
+                                                                            name = input('Кто создатель python? ')
+                                                                            if name == 'Гвидо':
+                                                                                print('Правильно!')
+                                                                            else:
+                                                                                print('Неверно')
