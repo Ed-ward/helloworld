430a52639decf14ee19a1ee8c7665694925892fd
EdwardElensky <elensky_ed516@mail.ru>
2021-01-18 22:20:44 +0300

    add range winners

---
+++ range_winners.py
@@ -0,0 +1,9 @@
+winners = ['Leo', 'Max', 'Kate']
+
+# используем  range
+for i in range(len(winners)):
+#    print(i)
+#    print(winners[i])
+# удобнее чем  while
+
+    print(i+1, ')', winners[i])
