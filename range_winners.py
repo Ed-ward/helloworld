194015392449a0e4b984648740f5192820e81229
EdwardElensky <elensky_ed516@mail.ru>
2021-01-19 22:45:32 +0300

    add range with param

--- range_winners.py
+++ range_winners.py
@@ -7,3 +7,10 @@ for i in range(len(winners)):
 # удобнее чем  while
 
     print(i+1, ')', winners[i])
+
+# upd
+# применение range с параметрами
+
+for i in range(1, len(winners)+1):
+    print(i, ')', winners[i-1])
+# ничего особо не улучшилось, зато есть альтернатива :)
