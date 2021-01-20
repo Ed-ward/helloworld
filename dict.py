fe573b658ec677554b9fa45216efb1e15de33979
EdwardElensky <elensky_ed516@mail.ru>
2021-01-20 23:52:00 +0300

    add dict

---
+++ dict.py
@@ -0,0 +1,22 @@
+# словарь - dict
+# неупорядоченные коллекции произвольных объектов с доступом по ключу.
+
+# my_dict = {key1: val1, key2: val2, ...}
+
+dog = {'name': 'Rocky', 'age': 3}
+
+
+friends = ['Leo', 'Max', 'Kate']
+print(friends)
+print(type(friends))
+friend = friends[0]
+print(friend)
+print(type(friend))
+
+# как добавить к имени еще параметр - можно использовать словарь
+friend = {
+    'name': 'Max',
+    'age': 23
+}
+print(friend)
+print(type(friend))
