cf0bad1415279956a8b7ea164d71b66247a19113
EdwardElensky <elensky_ed516@mail.ru>
2021-02-06 23:58:27 +0300

    add count

--- rand_game-st-5.py
+++ rand_game-st-5.py
@@ -6,7 +6,17 @@ print(number)
 
 # улучшаем цикл (выполняется до равенства чисел)
 user_number = None
+count = 0
+levels = {1: 10, 2: 5, 3: 3}
+level = int(input('Введите уровень сложности от 1 до 3: '))
+max_count = levels[level]
+
 while number != user_number:
+    count += 1
+    if count > max_count:
+        print('Вы проиграли')
+        break
+    print(f'Попытка № {count}')
     user_number = int(input('Введи число от 1 до 100: '))
     print(user_number)
 
@@ -14,5 +24,5 @@ while number != user_number:
         print('Ваше число больше загаданного!')
     elif number > user_number:
         print('Ваше число меньше загаданного!')
-
-print('Победа! Вы угадали!')
+else:
+    print('Победа! Вы угадали!')
